126ad5541ab71b268bee81ced21a4b44644a927b
Bolun Dai <bd1555@nyu.edu>
2022-11-14 10:56:04 -0500

    added code base for inverse dynamics controller

---
+++ franka_example_controllers/include/franka_example_controllers/inverse_dynamics_controller.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/parsers/urdf.hpp>
+#include <pinocchio/algorithm/joint-configuration.hpp>
+#include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/kinematics.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/aba.hpp>
+#include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/crba.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/algorithm/model.hpp>
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+
+#include <franka_hw/franka_cartesian_command_interface.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+#include <franka_hw/trigger_rate.h>
+
+namespace franka_example_controllers {
+
+class InverseDynamicsController : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface, 
+                                                                           hardware_interface::EffortJointInterface, 
+                                                                           franka_hw::FrankaStateInterface> {
+ public:
+  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
+
+ private:
+  // pinocchio model & data
+  pinocchio::Model model;
+  pinocchio::Data data;
+
+  // interface with franka_hw
+  std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
+  std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+  std::vector<hardware_interface::JointHandle> joint_handles_;
+};
+
+}  // namespace franka_example_controllers
---
+++ franka_example_controllers/src/inverse_dynamics_controller.cpp
@@ -0,0 +1,122 @@
+#include <franka_example_controllers/inverse_dynamics_controller.h>
+
+#include <cmath>
+
+#include <controller_interface/controller_base.h> 
+#include <hardware_interface/hardware_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>   
+
+namespace pin = pinocchio;
+
+namespace franka_example_controllers {
+
+bool InverseDynamicsController::init(hardware_interface::RobotHW* robot_hw,
+                                          ros::NodeHandle& node_handle) {
+  // check if got arm_id
+  std::string arm_id;
+  if (!node_handle.getParam("arm_id", arm_id)) {
+    ROS_ERROR_STREAM("InverseDynamicsController: Could not read parameter arm_id");
+    return false;
+  }
+  
+  // check if got joint_names
+  std::vector<std::string> joint_names;
+  if (!node_handle.getParam("joint_names", joint_names)) {
+    ROS_ERROR("InverseDynamicsController: Could not parse joint names");
+  }
+  if (joint_names.size() != 7) {
+    ROS_ERROR_STREAM("InverseDynamicsController: Wrong number of joint names, got " << joint_names.size() << " instead of 7 names!");
+    return false;
+  }
+  
+  // check if model_handle_ works
+  auto* model_interface = robot_hw->get<franka_hw::FrankaModelInterface>();
+  
+  if (model_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "InverseDynamicsController: Error getting model interface from hardware");
+    return false;
+  }
+
+  try {
+    model_handle_ = std::make_unique<franka_hw::FrankaModelHandle>(
+        model_interface->getHandle(arm_id + "_model"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "InverseDynamicsController: Exception getting model handle from interface: "
+        << ex.what());
+    return false;
+  }
+
+  // check if state_handle_ works
+  auto* state_interface = robot_hw->get<franka_hw::FrankaStateInterface>();
+  
+  if (state_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "InverseDynamicsController: Error getting state interface from hardware");
+    return false;
+  }
+  
+  try {
+    state_handle_ = std::make_unique<franka_hw::FrankaStateHandle>(
+        state_interface->getHandle(arm_id + "_robot"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "InverseDynamicsController: Exception getting state handle from interface: "
+        << ex.what());
+    return false;
+  }
+
+  // check if joint_handles_ got each joint (crucial step of avoiding could not switch controller error)
+  auto* effort_joint_interface = robot_hw->get<hardware_interface::EffortJointInterface>();
+  
+  if (effort_joint_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "InverseDynamicsController: Error getting effort joint interface from hardware");
+    return false;
+  }
+  
+  for (size_t i = 0; i < 7; ++i) {
+    try {
+      joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
+    } catch (const hardware_interface::HardwareInterfaceException& ex) {
+      ROS_ERROR_STREAM(
+          "InverseDynamicsController: Exception getting joint handles: " << ex.what());
+      return false;
+    }
+  }
+
+  // build pin_robot from urdf
+  std::string urdf_filename = "/home/bolun/bolun_ws/src/franka_ros_bolun/franka_example_controllers/fr3.urdf";
+  pin::urdf::buildModel(urdf_filename, model);
+  data = pin::Data(model);
+
+  return true;
+}
+
+void InverseDynamicsController::starting(const ros::Time& /* time */) {
+    // get intial robot state
+    franka::RobotState initial_state = state_handle_->getRobotState();
+}
+
+void InverseDynamicsController::update(const ros::Time& /*time*/, const ros::Duration& period) {
+  // get joint angles and angular velocities
+  franka::RobotState robot_state = state_handle_->getRobotState();
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
+
+  // update pinocchio robot model
+  pin::forwardKinematics(model, data, q, dq);
+  pin::updateFramePlacements(model, data); 
+
+  // set torque
+  for (size_t i = 0; i < 7; ++i) {
+    joint_handles_[i].setCommand(0.0 - dq[i]);
+  }
+}
+
+}  // namespace franka_example_controllers
+
+PLUGINLIB_EXPORT_CLASS(franka_example_controllers::InverseDynamicsController, controller_interface::ControllerBase)
